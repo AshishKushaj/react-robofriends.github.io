d94b697d40bb0ccc134f6f6f1c0c75ffec9b35f8
Ashish Kushaj <ashishkushajak@gmail.com>
2021-12-05 17:40:35 +0530

    added multiple cards

--- src/Card.js
+++ src/Card.js
@@ -4,10 +4,10 @@ const Card = ({id, name , email }) =>{
     return (
         <div className ="tc bg-light-green dib br3 pa3 ma3 grow bw2 shadow-5">
             <img src ={`https://robohash.org/${name}?set=set4`} alt ="robots"/>
-        <div>
-            <h2>{name}</h2>
-            <p>{email}</p>
-        </div>
+            <div>
+                <h2>{name}</h2>
+                <p>{email}</p>
+            </div>
         </div>
     );
     // tc - text center
--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import Card from "./Card";
-import {robots} from './robots';
 import "tachyons";
+import App from './App';
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
-  <div>
-    <Card id={robots[0].id} name={robots[0].name} email= {robots[0].email} />
-    <Card id={robots[1].id} name={robots[1].name} email= {robots[1].email} />
-    <Card id={robots[2].id} name={robots[2].name} email= {robots[2].email} />
-  </div>,
+  <App/>,
   document.getElementById("root")
 );
 
