ee031a5cf629ca4e65aec6994ea47e5a87e60097
Ashish Kushaj <ashishkushajak@gmail.com>
2021-12-05 15:28:49 +0530

    deleted default react app and added robo/cat app

---
+++ src/Card.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Card = ({id, name , email }) =>{
+    return (
+        <div className ="tc bg-light-green dib br3 pa3 ma3 grow bw2 shadow-5">
+            <img src ={`https://robohash.org/${name}?set=set4`} alt ="robots"/>
+        <div>
+            <h2>{name}</h2>
+            <p>{email}</p>
+        </div>
+        </div>
+    );
+    // tc - text center
+        // bg -back ground
+        // dib - display in block
+        // br - border radius
+        // pa - padding all (pr- padding right , pl- padding left, ... etc)
+        // ma - margin all
+        // grow - animation
+        // bw - border width
+        // shadow - shadow
+}
+export default Card;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Card from "./Card";
+import {robots} from './robots';
+import "tachyons";
+import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <div>
+    <Card id={robots[0].id} name={robots[0].name} email= {robots[0].email} />
+    <Card id={robots[1].id} name={robots[1].name} email= {robots[1].email} />
+    <Card id={robots[2].id} name={robots[2].name} email= {robots[2].email} />
+  </div>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
